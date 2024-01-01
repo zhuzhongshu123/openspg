dade23c95cae06f4b0ba1c12fa05b0369b390f27
baifuyu <fuyu.bfy@antgroup.com>
2024-01-01 20:38:52 +0800

    bugfix

--- dev/release/python/buildx-release-python.sh
+++ dev/release/python/buildx-release-python.sh
@@ -1,4 +1,4 @@
 docker buildx build -f Dockerfile --platform linux/arm64/v8,linux/amd64 --push \
-  -t openspg/openspg-mysql:0.0.2-beta1 \
-  -t openspg/openspg-mysql:latest \
+  -t openspg/openspg-python:0.0.2-beta1 \
+  -t openspg/openspg-python:latest \
   .
--- dev/release/server/buildx-release-server.sh
+++ dev/release/server/buildx-release-server.sh
@@ -1,4 +1,4 @@
 docker buildx build -f Dockerfile --platform linux/arm64/v8,linux/amd64 --push \
-  -t openspg/openspg-server:0.0.1-beta0101 \
+  -t openspg/openspg-server:0.0.2-beta1 \
   -t openspg/openspg-server:latest \
   .
